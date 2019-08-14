2b6889e868b1dfcd5e0dbcfbbaaad78ebb675306
Jibi Jose <jibijose@yahoo.com>
2019-08-14 10:29:08 +0530

    swagger2 added

--- src/main/java/com/jibi/SpringBootWebApplication.java
+++ src/main/java/com/jibi/SpringBootWebApplication.java
@@ -3,9 +3,9 @@ package com.jibi;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@Slf4j
 public class SpringBootWebApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootWebApplication.class, args);
