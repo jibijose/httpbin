3f663d97f893d626d3b6270546ad2bfaa34e2de9
Jibi Jose <jibijose@yahoo.com>
2019-08-16 01:04:32 +0530

    String util tests

---
+++ src/test/java/com/jibi/common/StringUtilTest.java
@@ -0,0 +1,24 @@
+package com.jibi.common;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.BlockJUnit4ClassRunner;
+
+@RunWith(BlockJUnit4ClassRunner.class)
+public class StringUtilTest {
+
+    @Test
+    public void testGetRandomString() {
+        assertEquals("Should return random string of expected length", 10, StringUtil.getRandomString(10).length());
+    }
+
+    @Test
+    public void testGetDifferentRandomStrings() {
+        String randomString1 = StringUtil.getRandomString(10);
+        String randomString2 = StringUtil.getRandomString(10);
+        assertNotEquals("Should return different strings", randomString1, randomString2);
+    }
+}
