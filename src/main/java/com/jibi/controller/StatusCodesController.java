4e2349b8f69757441b15c8c91444deefd776804f
Jibi Jose <jibijose@yahoo.com>
2019-08-17 20:20:00 +0530

    Status code random

--- src/main/java/com/jibi/controller/StatusCodesController.java
+++ src/main/java/com/jibi/controller/StatusCodesController.java
@@ -9,6 +9,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
 @RestController
 @Api(value = "Status codes Api")
 public class StatusCodesController {
@@ -17,6 +23,17 @@ public class StatusCodesController {
     @RequestMapping(value = "/status/{code}", method = RequestMethod.GET)
     public ResponseEntity<Void> statusCode(@PathVariable("code") Integer code) {
         HttpStatus httpStatus = HttpStatus.valueOf(code);
+        return new ResponseEntity<Void>(httpStatus);
+    }
+
+    @ApiOperation(value = "Status random code api", response = Void.class)
+    @RequestMapping(value = "/status/random/{codes}", method = RequestMethod.GET)
+    public ResponseEntity<Void> statusRandomCodes(@PathVariable("codes") String strCodes) {
+        String[] arrayCodes = strCodes.split(",");
+        List<Integer> codes = Arrays.stream(arrayCodes).map(str -> Integer.parseInt(str)).collect(Collectors.toList());
+        Random random = new Random(new Date().getTime());
+        Integer randomRangeIndex = random.ints(0, arrayCodes.length).limit(1).findFirst().getAsInt();
+        HttpStatus httpStatus = HttpStatus.valueOf(codes.get(randomRangeIndex));
         return new ResponseEntity<Void>(httpStatus);
     }
 }
--- src/test/java/com/jibi/controller/StatusCodesControllerTest.java
+++ src/test/java/com/jibi/controller/StatusCodesControllerTest.java
@@ -10,8 +10,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.anyOf;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -24,19 +25,19 @@ public class StatusCodesControllerTest {
     private TestRestTemplate restTemplate;
 
     @Test
-    public void test200StatusCodes() throws Exception {
+    public void test200StatusCode() throws Exception {
         ResponseEntity<Void> responseEntity = restTemplate.getForEntity("http://localhost:" + port + "/status/200", Void.class);
         assertEquals(HttpStatus.OK.value(), responseEntity.getStatusCodeValue());
     }
 
     @Test
-    public void test400StatusCodes() throws Exception {
+    public void test400StatusCode() throws Exception {
         ResponseEntity<Void> responseEntity = restTemplate.getForEntity("http://localhost:" + port + "/status/400", Void.class);
         assertEquals(HttpStatus.BAD_REQUEST.value(), responseEntity.getStatusCodeValue());
     }
 
     @Test
-    public void test500StatusCodes() throws Exception {
+    public void test500StatusCode() throws Exception {
         ResponseEntity<Void> responseEntity = restTemplate.getForEntity("http://localhost:" + port + "/status/500", Void.class);
         assertEquals(HttpStatus.INTERNAL_SERVER_ERROR.value(), responseEntity.getStatusCodeValue());
     }
@@ -44,6 +45,18 @@ public class StatusCodesControllerTest {
     @Test
     public void testIncorrectStatusCode() throws Exception {
         ResponseEntity<Void> responseEntity = restTemplate.getForEntity("http://localhost:" + port + "/status/999", Void.class);
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR.value(), responseEntity.getStatusCodeValue());
+    }
+
+    @Test
+    public void test200400500StatusRandomCode() throws Exception {
+        ResponseEntity<Void> responseEntity = restTemplate.getForEntity("http://localhost:" + port + "/status/random/200,400,500", Void.class);
+        org.hamcrest.MatcherAssert.assertThat(responseEntity.getStatusCodeValue(), anyOf(is(200), is(400), is(500)));
+    }
+
+    @Test
+    public void test999StatusRandomCode() throws Exception {
+        ResponseEntity<Void> responseEntity = restTemplate.getForEntity("http://localhost:" + port + "/status/random/999", Void.class);
         assertEquals(HttpStatus.INTERNAL_SERVER_ERROR.value(), responseEntity.getStatusCodeValue());
     }
 }
