f2cc2354a3444318e435aa3ccc7d0c61012f6a73
Jibi Jose <jibijose@yahoo.com>
2019-10-28 11:35:03 +0530

    upload test

---
+++ src/main/java/com/jibi/controller/UploadDownloadController.java
@@ -0,0 +1,43 @@
+package com.jibi.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Api(value = "Upload Download Api")
+@RestController(value = "Upload Download Api")
+public class UploadDownloadController {
+
+    @ApiOperation(value = "Upload api", response = String.class)
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Ok"),
+            @ApiResponse(code = 500, message = "Internal server error")})
+    @RequestMapping(value = "/upload", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public Map<String, String> upload(@RequestParam("file") MultipartFile file) throws IOException {
+        Map<String, String> uploadStatus = new HashMap<>();
+        byte[] bytes = file.getBytes();
+        uploadStatus.put("uploadedBytesLength", Integer.toString(bytes.length));
+        uploadStatus.put("contentType", file.getContentType());
+        uploadStatus.put("name", file.getName());
+        uploadStatus.put("originalFileName", file.getOriginalFilename());
+        uploadStatus.put("size", Long.toString(file.getSize()));
+        if ( file.getSize() == bytes.length ) {
+            uploadStatus.put("status", "success");
+        }
+
+        return uploadStatus;
+    }
+
+}
---
+++ src/test/java/com/jibi/controller/UploadDownloadControllerTest.java
@@ -0,0 +1,45 @@
+package com.jibi.controller;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import java.util.Map;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class UploadDownloadControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void testUpload() throws Exception {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+        body.add("file", new ClassPathResource("logback-test.xml"));
+
+        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
+
+        ResponseEntity<Map> response = this.restTemplate.postForEntity("http://localhost:" + port + "/upload", requestEntity, Map.class);
+        Assert.assertEquals("Status should be success", "success", response.getBody().get("status"));
+    }
+
+}
