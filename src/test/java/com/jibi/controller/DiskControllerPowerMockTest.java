c4924cddc45e164f7099713bdee7c4d44d1edb45
Jibi Jose <jibijose@yahoo.com>
2019-11-08 19:25:00 +0530

    jdk 11 build correction

--- src/test/java/com/jibi/controller/DiskControllerPowerMockTest.java
+++ src/test/java/com/jibi/controller/DiskControllerPowerMockTest.java
@@ -9,11 +9,19 @@ import java.io.IOException;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(DiskController.class)
+@PowerMockIgnore({
+  "com.sun.org.apache.xalan.*",
+  "com.sun.org.apache.xerces.*",
+  "javax.xml.*",
+  "org.xml.*",
+  "org.w3c.*"
+})
 public class DiskControllerPowerMockTest {
 
   private DiskController diskController;
