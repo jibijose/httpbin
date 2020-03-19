ba8490674db2cf26f7ab9b21b48422682305b083
Jibi Jose <jibijose@yahoo.com>
2020-03-20 02:34:47 +0530

    batch gc

--- src/main/java/com/jibi/HttpBinApplication.java
+++ src/main/java/com/jibi/HttpBinApplication.java
@@ -3,13 +3,29 @@ package com.jibi;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
 @Slf4j
+@EnableScheduling
 public class HttpBinApplication {
   public static void main(String[] args) {
     log.debug("Starting httpbin application");
     SpringApplication.run(HttpBinApplication.class, args);
     log.debug("Ending httpbin application");
+  }
+
+  @Scheduled(cron = "0 */1 * * * ?")
+  public void perform() throws Exception {
+    log.error("Invoking System GC");
+    System.out.println("Doing");
+    try {
+      System.gc();
+      System.out.println("Done");
+      log.info("Invoked System GC");
+    } catch(Exception exception) {
+      log.warn("Errored System GC", exception);
+    }
   }
 }
