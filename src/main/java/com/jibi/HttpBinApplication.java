fca3ff517c698b41899d24c6f117d5ff0c6daddc
Jibi Jose <jibijose@yahoo.com>
2020-03-22 07:53:36 +0530

    gc batch removed

--- src/main/java/com/jibi/HttpBinApplication.java
+++ src/main/java/com/jibi/HttpBinApplication.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
 @Slf4j
@@ -16,14 +15,4 @@ public class HttpBinApplication {
     log.debug("Ending httpbin application");
   }
 
-  @Scheduled(cron = "0 */5 * * * ?")
-  public void perform() throws Exception {
-    log.error("Invoking System GC");
-    try {
-      System.gc();
-      log.info("Invoked System GC");
-    } catch (Exception exception) {
-      log.warn("Errored System GC", exception);
-    }
-  }
 }
