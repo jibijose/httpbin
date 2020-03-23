af9a14dd90a057193ad8b1730a14cbc5bc79cc50
Jibi Jose <jibijose@yahoo.com>
2020-03-23 11:46:42 +0530

    config folder

--- scripts/dockerBuild.sh
+++ scripts/dockerBuild.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-mvn clean package
-docker rmi jibijose/httpbin:latest
+mvn clean package spotbugs:check fmt:check checkstyle:check pmd:check
+#docker stop $(docker ps -a -q)
+#docker rm $(docker ps -a -q)
+docker  rmi jibijose/httpbin:latest
 docker build --build-arg MVN_VERSION=3.6.3 -t jibijose/httpbin:latest .
