5e787abf01e9e30848ad5b2f21ab8201778a2c60
Jibi Jose <jibijose@yahoo.com>
2020-03-24 19:23:32 +0530

    docker separation

--- scripts/dockerBuild.sh
+++ scripts/dockerBuild.sh
@@ -4,4 +4,4 @@ mvn clean package spotbugs:check fmt:check checkstyle:check pmd:check site
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 docker  rmi jibijose/httpbin:latest
-docker build --build-arg MVN_VERSION=3.6.3 -t jibijose/httpbin:latest .
+docker build --build-arg MVN_VERSION=3.6.3 -t jibijose/httpbin:latest -f docker/jdk8/Dockerfile .
