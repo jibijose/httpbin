224043ac759ce97ff7862cb9eaf9f408b32969ee
Jibi Jose <jibijose@yahoo.com>
2020-03-28 19:02:02 +0530

    corrections

--- scripts/dockerBuild.sh
+++ scripts/dockerBuild.sh
@@ -3,5 +3,6 @@
 mvn clean package spotbugs:check fmt:check checkstyle:check pmd:check site
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
+#docker rmi $(docker images -a -q)
 docker  rmi jibijose/httpbin:latest
-docker build --build-arg MVN_VERSION=3.6.3 -t jibijose/httpbin:latest -f docker/jdk8/Dockerfile .
+docker build --build-arg MVN_VERSION=3.6.3 -t jibijose/httpbin:latest -f docker/jre8/Dockerfile .
