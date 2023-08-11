c9326dedbd135b87b280b445f1bc9449fc951590
Jibi Jose <jibijose@yahoo.com>
2023-08-11 14:30:54 +0530

    codacy 1

--- src/test/java/com/jibi/HttpBinApplicationApplicationTest.java
+++ src/test/java/com/jibi/HttpBinApplicationApplicationTest.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 /** The type Http bin application application test. */
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class HttpBinApplicationApplicationTest {
+class HttpBinApplicationApplicationTest {
 
   /**
    * Context loads.
@@ -13,7 +13,7 @@ public class HttpBinApplicationApplicationTest {
    * @throws Exception the exception
    */
   @Test
-  public void contextLoads() throws Exception {}
+  void contextLoads() throws Exception {}
 
   /**
    * Test main.
@@ -21,7 +21,7 @@ public class HttpBinApplicationApplicationTest {
    * @throws Exception the exception
    */
   @Test
-  public void testMain() throws Exception {
+  void testMain() throws Exception {
     HttpBinApplication.main(new String[] {});
   }
 }
