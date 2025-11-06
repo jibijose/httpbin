53b2fcee66097c1cc44761be985b07dc2d2ef4c7
Jibi Jose <jibijose@yahoo.com>
2025-11-06 08:21:45 +0530

    docker build changes

--- scripts/dockerBuild.sh
+++ scripts/dockerBuild.sh
@@ -6,4 +6,6 @@ docker rm $(docker ps -a -q)
 #docker rmi $(docker images -a -q)
 docker  rmi jibijose/httpbin:jdk17-latest
 
-#docker build --build-arg MVN_VERSION=3.9.6 -t jibijose/httpbin:jdk17-latest -f docker/jdk17/Dockerfile .
+#docker build --build-arg MVN_VERSION=3.9.11 -t jibijose/httpbin:jdk17-latest -f docker/jdk17/Dockerfile .
+#docker build --build-arg MVN_VERSION=3.9.11 -t jibijose/httpbin:jdk21-latest -f docker/jdk21/Dockerfile .
+#docker build --build-arg MVN_VERSION=3.9.11 -t jibijose/httpbin:jdk24-latest -f docker/jdk24/Dockerfile .
