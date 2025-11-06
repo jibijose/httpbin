db65c06189b0648da8ee1ccdf69474a5be633cde
Jibi Jose <jibijose@yahoo.com>
2025-11-06 08:29:33 +0530

    3.9.9 + 3.9.11

--- scripts/tagReleaseVersion.sh
+++ scripts/tagReleaseVersion.sh
@@ -18,8 +18,8 @@ docker login registry.hub.docker.com
 
 echo "*****************************************************************************************"
 echo "Building docker repository"
-docker build --build-arg MVN_VERSION=3.9.9 -t jibijose/httpbin:jre17-latest -f docker/jre17/Dockerfile .
-docker build --build-arg MVN_VERSION=3.9.9 -t jibijose/httpbin:jdk17-latest -f docker/jdk17/Dockerfile .
+docker build --build-arg MVN_VERSION=3.9.11 -t jibijose/httpbin:jre17-latest -f docker/jre17/Dockerfile .
+docker build --build-arg MVN_VERSION=3.9.11 -t jibijose/httpbin:jdk17-latest -f docker/jdk17/Dockerfile .
 
 docker tag jibijose/httpbin:jre17-latest jibijose/httpbin:jre17-${VERSION}
 docker tag jibijose/httpbin:jdk17-latest jibijose/httpbin:jdk17-${VERSION}
--- scripts/testEndpoints.sh
+++ scripts/testEndpoints.sh
@@ -14,13 +14,13 @@ docker stop $(docker ps -aq -f ancestor=jibijose/httpbin:latest)
 docker rm $(docker ps -aq -f ancestor=jibijose/httpbin:latest)
 
 docker  rmi jibijose/httpbin:latest
-docker build --build-arg MVN_VERSION=3.9.9 -t jibijose/httpbin:latest -f docker/jdk17/Dockerfile .
+docker build --build-arg MVN_VERSION=3.9.11 -t jibijose/httpbin:latest -f docker/jdk17/Dockerfile .
 docker run -p 8080:8080 jibijose/httpbin:latest
 
 curl -D http://localhost:8080/health
 curl -D http://localhost:8080/cpu/all/50/10
 ---------------------------------------------------
-docker build --build-arg MVN_VERSION=3.9.9 -t jibijose/httpbin:jdk24-latest -f docker/jdk24/Dockerfile .
+docker build --build-arg MVN_VERSION=3.9.11 -t jibijose/httpbin:jdk24-latest -f docker/jdk24/Dockerfile .
 docker run -p 8080:8080 jibijose/httpbin:jdk24-latest
 
 docker run -it jibijose/httpbin:jdk24-latest /bin/bash
