7c0f2115feb68ce5268e46df2b9f3161dbfafaad
Jibi Jose <jibijose@yahoo.com>
2025-12-23 14:35:01 +0530

    gradle versions

--- src/test/java/com/jibi/config/TestConfig.java
+++ src/test/java/com/jibi/config/TestConfig.java
@@ -1,7 +1,6 @@
 package com.jibi.config;
 
 import java.io.IOException;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpResponse;
@@ -9,7 +8,6 @@ import org.springframework.web.client.ResponseErrorHandler;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-@Slf4j
 public class TestConfig {
 
   @Bean
